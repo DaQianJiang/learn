03ca607f7e97affe42435abd6a5ca23004104490
DaQianJiang <1404482005@qq.com>
2020-09-05 22:19:10 +0800

    update the LearnApplication

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -3,16 +3,33 @@ package com.example.learn;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
 //@MapperScan("com.example.learn.mapper")
-public class LearnApplication {
+//public class LearnApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(LearnApplication.class, args);
+//    }
+//
+//
+//
+//}
 
-    public static void main(String[] args) {
+public class LearnApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearnApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(LearnApplication.class, args);
     }
 
 
 
 }
+
